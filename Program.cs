0a42d39a8c5dade0975ee93756968c7bc204bbe2
Andres Sainz <andresmsainz@gmail.com>
2021-04-24 17:36:36 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ReadLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name?");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Welcome " + name + " welcome back, man!");
+        }
+    }
+}
